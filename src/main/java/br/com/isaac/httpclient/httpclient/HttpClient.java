410486da06cbb5d05213fd1289b41ba5dcf76db0
isaacsilvatech <isaacsilva.tech@gmail.com>
2023-10-12 11:47:20 -0300

    adicionado metodo com function para facilitar a criacao de novos metodos

--- src/main/java/br/com/isaac/httpclient/httpclient/HttpClient.java
+++ src/main/java/br/com/isaac/httpclient/httpclient/HttpClient.java
@@ -1,22 +1,28 @@
 package br.com.isaac.httpclient.httpclient;
 
 import java.net.HttpURLConnection;
+import java.util.function.Function;
 
 import br.com.isaac.httpclient.httpclient.reactive.Observable;
 import br.com.isaac.httpclient.httpclient.reactive.Subject;
 
 public class HttpClient {
-	
+
 	private HttpClientExecutor executor = new HttpClientExecutor();
 
 	public <T> Observable<T> get(String apiUrl, Class<T> clazz) {
+
+		return request(r -> r.get(apiUrl), clazz);
+	}
+
+	private <T> Observable<T> request(Function<Resquest, Response> function, Class<T> clazz) {
 		
 		Subject<T> subject = new Subject<T>();
-		
+
 		executor.toRequest(() -> {
 			Resquest request = new Resquest();
-			Response response = request.get(apiUrl);
-			if(response.getStatus() == HttpURLConnection.HTTP_OK) {
+			Response response = function.apply(request);
+			if (response.getStatus() == HttpURLConnection.HTTP_OK) {
 				T t = response.parse(clazz);
 				subject.next(t);
 			} else {
